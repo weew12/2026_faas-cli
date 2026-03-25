c6d238a9718293bf7e08ad6404c69a932f08ccb6
weew12 <weew12@2528566339@qq.com>
2026-03-25 05:49:24 +0000

    3 构建 OpenFaaS 函数 + 查看镜像

--- build_samples.sh
+++ build_samples.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# 构建 OpenFaaS 函数 + 查看镜像
+
+# 1. 调用 faas-cli 执行【构建函数镜像】
+# 等价于：faas-cli build -f stack.yml
+# 会根据当前目录的 yaml 文件构建 Docker 镜像
 ./bin/faas-cli build # --squash=true
 
+# 2. 列出本地所有 Docker 镜像，只显示前 4 行（方便查看刚构建的函数镜像）
 docker images |head -n 4
